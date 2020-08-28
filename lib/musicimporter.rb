3e317f613699ab86221e35d440dc67c54899fabc
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2020-08-28 01:59:47 +0000

    Automatically backed up by Learn

--- lib/musicimporter.rb
+++ lib/musicimporter.rb
@@ -2,29 +2,23 @@ require 'pry'
 
 class MusicImporter
 
-  attr_accessor :path
-
-  @@files = []
+  attr_accessor :path, :files
 
   def initialize(path)
     @path = path
+    @files = []
   end
 
   def files
-    binding.pry
     files = Dir[@path + "/**/*.mp3"]
-    binding.pry
     files.map! {|file| file.gsub(@path + "/","")}
-    binding.pry
-    files.each {|file| @@files << file} unless @@files.include?(file)
-    binding.pry
-    @@files
+    files.each {|file| @files << file unless @files.include?(file)}
+    @files
   end
 
   def import
-    binding.pry
-    @@files.each {|file| Song.create_from_filename(file)}
-    binding.pry
+    files
+    @files.each {|file| Song.create_from_filename(file)}
   end
 
 end
